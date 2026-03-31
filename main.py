ea2cd2c2fbb50b11f442048dba447daebe3bf6c4
Eduardo Cansan <eduardocansann@gmail.com>
2026-03-30 21:27:47 -0300

    fix: ajust simple variables and add comment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Librarys for better output in terminal 
 from rich.console import Console
 from rich.table import Table
 
 console = Console()
 
+#Function to facilitate output menu
 def show_main_menu():
     table = Table(title="\nEncode or Decode")
     table.add_column("OPTION 1", style="steel_blue1")
@@ -10,32 +12,32 @@ def show_main_menu():
     table.add_row("Encode", "Decode")
     console.print(table)
 
-
+#Function to facilitate output methods
 def show_methods_menu(action: str):
     table = Table(title=f"\nMethods — {action}")
-    for col, method in [("A", "Golomb"), ("B", "Elias-Gamma"), ("C", "Fibonacci"), ("D", "Huffman")]:
+    for col, method in [("1", "Golomb"), ("2", "Elias-Gamma"), ("3", "Fibonacci"), ("4", "Huffman")]:
         table.add_column(f"OPTION {col}", style="khaki1")
     table.add_row("Golomb", "Elias-Gamma", "Fibonacci", "Huffman")
     console.print(table)
 
-
+#Methods for this project
 METHODS = {
-    "A": "Golomb",
-    "B": "Elias-Gamma",
-    "C": "Fibonacci",
-    "D": "Huffman",
+    "1": "Golomb",
+    "2": "Elias-Gamma",
+    "3": "Fibonacci",
+    "4": "Huffman",
 }
 
-
+#Better output for showing methods
 def handle_action(action: str):
     show_methods_menu(action)
     choice = input(f"Choose a method to {action.lower()}: ").upper()
     if choice in METHODS:
         console.print(f"[bold green]{action}ing with {METHODS[choice]}...[/bold green]")
     else:
-        console.print("[bold red]Invalid option![/bold red]")
+        console.print("\n[bold red]Invalid option![/bold red]")
 
-
+#Main method to run everything
 def main():
     show_main_menu()
     option = input("Enter the option: ").strip()
@@ -45,8 +47,7 @@ def main():
     elif option == "2":
         handle_action("Decode")
     else:
-        console.print("[bold red]Invalid option![/bold red]")
-
+        console.print("\n[bold red]Invalid option![/bold red]")
 
 if __name__ == "__main__":
     main()
