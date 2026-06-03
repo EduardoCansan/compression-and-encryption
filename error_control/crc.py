bc95902ca9007165b6a546d8bf3ee368f080d02c
gusmezomo <gusmezomo@gmail.com>
2026-06-02 21:03:12 -0300

    crc v4 - separando defs

--- error_control/crc.py
+++ error_control/crc.py
@@ -17,71 +17,72 @@ class crc_generator:
             
         return resultado_xor
 
-    # # 1. Criamos a instância da classe
-    # meu_gerador = crc_generator()
-    # # 2. Chamamos a função passando os dados e usando os parênteses ()
-    # resultado_final = meu_gerador.calcular_crc(meu_gerador.entrada, meu_gerador.gerador_crc)
-    # print("Resultado do XOR:", resultado_final)
+    def divisao_crc(self, entrada):
 
-    def calcular_crc(self, ):
-        bits_reservados = len(self.gerador_crc) - 1
-        crc_com_bits = self.entrada + ("0" * bits_reservados)
-        #Entrada + 0s:  1011000000
-        
-        # print("Entrada + 0s: ", crc_com_bits)
-        
-        #10110
-        bloco_atual = crc_com_bits[:len(self.gerador_crc)]
-        
-        #0
+        bloco_atual = entrada[:len(self.gerador_crc)]
+
         i_proximo_bit = len(self.gerador_crc)
-        
-        # print(bloco_atual)
-        # print(i_proximo_bit)
-        
-        while i_proximo_bit < len(crc_com_bits):
+
+        while i_proximo_bit < len(entrada):
+
             if bloco_atual[0] == str("1"):
                 resultado = self.funcao_xor(bloco_atual, self.gerador_crc)
             else:
                 resultado = bloco_atual
-            
-            while resultado[0] == str("0"): #! VERIFICAR, PODE QUEBRAR SE FOR SÓ 0s, 00000
+
+            while len(resultado) > 0 and resultado[0] == str("0"):  #! VERIFICAR, PODE QUEBRAR SE FOR SÓ 0s, 00000
                 resultado = resultado[1:]
-            
+
             while len(resultado) < len(self.gerador_crc):
-                resultado = resultado + crc_com_bits[i_proximo_bit]
+                resultado = resultado + entrada[i_proximo_bit]
                 i_proximo_bit += 1
-            
+
             bloco_atual = resultado
-            
-            # print("Bloco:", bloco_atual)
-            # print("Indice:", i_proximo_bit)
-            
+
         if len(bloco_atual) == len(self.gerador_crc):
             resultado = self.funcao_xor(bloco_atual, self.gerador_crc)
-            while resultado[0] == str("0"): #! VERIFICAR, PODE QUEBRAR SE FOR SÓ 0s, 00000
+
+            while len(resultado) > 0 and resultado[0] == str("0"):  #! VERIFICAR, PODE QUEBRAR SE FOR SÓ 0s, 00000
                 resultado = resultado[1:]
+
             bloco_atual = resultado
-            # print("bloco final: ", bloco_atual)
-        else: 
+        else:
             return bloco_atual
-            
+
         return bloco_atual
+
+    def calcular_crc(self):
+        bits_reservados = len(self.gerador_crc) - 1
+        crc_com_bits = self.entrada + ("0" * bits_reservados)
+
+        return self.divisao_crc(crc_com_bits)
 
     def gerar_mensagem_crc(self):
         crc = self.calcular_crc()
         resultado_mensagem = self.entrada + crc
         return resultado_mensagem
-    
-    # def verificar_crc(self):
-    #     entrada_verf = self.gerar_mensagem_crc()
-        
-        
-#criado instacia da classe
+
+    def verificar_crc(self):
+        entrada_verf = self.gerar_mensagem_crc()
+
+        resultado = self.divisao_crc(entrada_verf)
+
+        if resultado == "":
+            print("CRC VALIDO")
+        else:
+            print("ERRO DETECTADO")
+            print("Resto:", resultado)
+
+        return resultado
+
+
+# criado instacia da classe
 meu_crc = crc_generator()
-#chama funcao
+
 crc = meu_crc.calcular_crc()
-print("CRC FINAL: ", crc)
+print("CRC FINAL:", crc)
 
 msg = meu_crc.gerar_mensagem_crc()
-print("mensagem crc: ", msg)
+print("mensagem crc:", msg)
+
+verificacao = meu_crc.verificar_crc()
