6499967ef2cc558c60233b0f48df478984801367
gusmezomo <gusmezomo@gmail.com>
2026-06-04 12:40:43 -0300

    feat: error.py v2 - def inverter and random

--- error_control/error.py
+++ error_control/error.py
@@ -1,39 +1,46 @@
+import random
 class error_simulator:
 
+    #apenas um caracter 
     def inverter_bit(self, entrada, posicao):
-        
-        caracter = entrada[posicao]
-        print("posicao: ", posicao , "bit: ", entrada[posicao])
-        
-        if caracter == "1":
-            caracter = "0"
+        if len(entrada) < posicao :
+            return "Error: Posicao escolhida é maior que a quantidade de caracteres da entrada!"
         else:
-            caracter = "1"
-        
-        palavra_antes = entrada[:posicao]
-        palavra_depois = entrada[posicao + 1:]
+            caracter = entrada[posicao]
+            print("posicao: ", posicao , "bit: ", entrada[posicao])
+            
+            if caracter == "1":
+                caracter = "0"
+            else:
+                caracter = "1"
+            
+            palavra_antes = entrada[:posicao]
+            palavra_depois = entrada[posicao + 1:]
 
-        palavra_final = palavra_antes + caracter + palavra_depois
+            palavra_final = palavra_antes + caracter + palavra_depois
 
-        return palavra_final
+            return palavra_final
 
     def inserir_erros_aleatorios(self, entrada, quantidade):
-        """
-        Inserção automática de erro.
-
-        Recebe:
-            bits -> mensagem binária
-            quantidade -> quantidade de erros a serem inseridos
-
-        Deve:
-            - escolher posições aleatórias da mensagem
-            - inverter os bits dessas posições
-            - evitar repetir a mesma posição (opcional)
-
-        Retorna:
-            mensagem binária modificada
-        """
-        pass
+        if len(entrada) < quantidade :
+            return "Error: Quantidade de posicoes escolhida, maior que a quantidade de caracteres da entrada"
+        else:
+            #quantidade = num d posicoes mudadas
+            posicoes = random.sample(range(len(entrada)), quantidade)
+            
+            lista_entrada = list(entrada)
+            
+            for i in posicoes:
+                if lista_entrada[i] == "1":
+                    lista_entrada[i] = "0"
+                else:
+                    lista_entrada[i] = "1"
+            
+            nova_msg = "".join(lista_entrada)
+            
+            saida = print(f"posicoes invertidas: {posicoes}", f"\n mensagem original:  {entrada}", f"\n mensagem modificada: {nova_msg}")
+            
+            return saida
 
     def gerar_posicoes_erro(self, tamanho, quantidade):
         """
@@ -55,8 +62,11 @@ class error_simulator:
         """
         pass
 
-#classe
+
 error = error_simulator()
 
-inverter = error.inverter_bit("10101111", 1)
+inverter = error.inverter_bit("10101101", 2)
 print(inverter)
+
+aleatorio = error.inserir_erros_aleatorios("10101101", 2)
+print(aleatorio)
